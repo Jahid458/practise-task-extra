db8379bd758a8c1ff75491ee5db7af0d345d7004
Jahid458 <Jahidul898@outlook.com>
2026-03-05 12:13:06 +0600

    bg hero section added and hero section complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,15 @@ import arrow from "/Arrow.png";
 import twitter from "/twitter.png";
 import print from "/printerest.svg";
 import fb from "/facebook.svg"
-//
+import basket from '/ball.svg'
 import cross from "/cross.png";
 
 const Home = () => {
   return (
-    <div className=" bg-black ">
-      <div className="container mx-auto h-120 lg:w-175 ">
+    <div className=" bg-black"
+    style={{backgroundImage:"url('/BgHero.png')"}}
+    >
+      <div className="container mx-auto h-96 lg:w-175 ">
         <div className="text-center  pt-14 space-y-6 ">
           <h1 className="lg:text-5xl font-effa text-4xl font-black text-white ">
             Minimize your tabs. <br /> Find the trends!
@@ -37,9 +39,13 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="flex">
-        <div className="text-white flex   mt-5 relative">
-          <div className="bg-[#181818] px-5 py-3 flex flex-wrap  items-center gap-3 relative -rotate-4 shadow-lg mb-12 my-5 z-10">
+          </div>
+
+      <div className=" flex justify-center items-center">
+        <div className="text-white flex mt-5 relative gap-5">
+          
+          <div className="bg-[#181818] px-8 py-3 flex flex-wrap  items-center
+           gap-3 relative -rotate-10 shadow-lg mb-12 my-8 z-50">
             <img src={twitter} className="w-7 h-7" />
             <h1 className="font-roboto flex items-center gap-1 relative text-white">
               Cryptopunk - Search
@@ -49,7 +55,9 @@ const Home = () => {
             </h1>
           </div>
 
-          <div className="bg-[#181818] px-5 py-3  flex flex-wrap items-center gap-3 -rotate-2 relative shadow-lg -ml-14 my-8 mt-9 z-0">
+
+          <div className="bg-[#181818] px-10 flex flex-wrap items-center gap-3 
+          rotate-4 relative shadow-lg -ml-16 my-10 z-30">
             <img src={print} className="w-7 h-7" />
             <h1 className="font-roboto flex  items-center gap-1 relative  text-white">
               Popular Design - Search
@@ -59,24 +67,30 @@ const Home = () => {
             </h1>
           </div>
 
-
-          <div className="bg-[#181818] px-6  flex flex-wrap items-center gap-3 -rotate-2 relative
-           shadow-lg -ml-7  my-10 mb-7  z-10">
+          <div className="bg-[#181818] px-7  flex flex-wrap items-center gap-3 -rotate-2 relative
+           shadow-lg -ml-12 -mt-0.5 mb-22 z-20">
             <img src={fb} className="w-7 h-7" />
             <h1 className="font-roboto flex items-center gap-1 relative  text-white">
-              Product Design - Search
+              Product Design -  Search
               <span className="ml-32 absolute border-gradient-to-r  text-gray-400 text-lg font-light cursor-pointer hover:text-gray-200 transition bg-[#181818] opacity-80">
                 <img src={cross} alt="" className="ml-7" />
               </span>
             </h1>  
           </div>
+
+          <div className="bg-[#181818] px-4 py-3 mb-16 mt-5 flex flex-wrap items-center gap-3 
+          rotate-10 relative shadow-lg -ml-12   z-10">
+            <img src={basket} className="w-7 h-7" />
+            <h1 className="font-roboto  flex items-center gap-1 relative  text-white pr-6">
+              Elon Musk - Search
+              <span className=" absolute right-0 border-gradient-to-r text-gray-400 text-lg
+               font-light cursor-pointer hover:text-gray-200 transition bg-[#181818] opacity-80">
+                <img src={cross}  className="w-5 h-5"  />
+              </span>
+            </h1>  
+          </div>  
         </div>
-       
         </div>
-
-
-
-      </div>
     </div>
   );
 };  
